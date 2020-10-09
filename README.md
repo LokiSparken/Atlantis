cecb8e47f441d414bfb9643cda8a22a27de9bee2
LokiSparken <sparken@qq.com>
2020-10-09 15:27:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Atlantis
+
+Developed with Unreal Engine 4
---
+++ Source/Atlantis/AtlantisGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AtlantisGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ATLANTIS_API AAtlantisGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
